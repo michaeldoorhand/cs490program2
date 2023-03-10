41f5a5c168b3429911e7b61f7b033e86e3d945a3
michaeldoorhand <72215840+michaeldoorhand@users.noreply.github.com>
2023-03-10 14:24:56 -0600

    Update program2.rkt
    complete !

--- program2.rkt
+++ program2.rkt
@@ -1,11 +1,15 @@
 #lang racket
 
+;opens file and returns as a string
 (define (read-file file-name) 
   (file->string file-name #:mode 'text))
 
+;gets rid of any occurance of 2 or more whitespace and any [ ] $ # , \ " ? ! _ - : ;
+;returns string
 (define (clean-file input-string)
   (string-downcase (regexp-replace* #px"\\s{2,}" (regexp-replace* #px"[[\\]$#,\".?!\n_\\-:;]" input-string " ") " ")))
 
+;creates a hash from a string with key being the word and the value the count of occurences of the word
 (define (string-to-hash input-string)
   (define (make-count-hash input-list final-hash)
     (if (empty? input-list) final-hash
@@ -15,20 +19,22 @@
           (make-count-hash (rest input-list) (hash-set final-hash word (+ (hash-ref final-hash word) 1)))
           (make-count-hash (rest input-list) (hash-set final-hash word 1)))))))
   (make-count-hash (regexp-split #rx" " input-string) (hash)))
-  
+
+;transforms the occurence hash into a hash of frequencies
 (define (make-frequency-hash input-hash)
   (let ([total (foldl + 0 (hash-values input-hash))])
   (hash-map/copy input-hash (lambda (k v) (values k (* (log (/ v total) 10) -1))))))
 
+;combines all of the above functions to create a rarity-hash from a file 
 (define (get-rarity-hash file-name)
   (make-frequency-hash (string-to-hash (clean-file (read-file file-name)))))
 
-(define (make-rarity-table file-list table-hash)
-  (if (empty? file-list) table-hash
-  (let ([file-name (first file-list)])
-    (make-rarity-table (rest file-list) (hash-set table-hash file-name (get-rarity-hash file-name))))))
-
-(define (analyze-text control-table input-mystery-table)
+;compares a known/control text to a mystery text
+;if the mystery text word is in the control perform the absolute value of the difference
+;else set the difference to 0
+;I tried not adding the word to the results if it wasn't in the control, but was getting incorrect
+;output due to the total amount of differences being lower/inaccurate
+(define (analyze-text control mystery)
   (define (compare-two control-hash mystery-hash results)
     (if (hash-empty? mystery-hash) results 
   (let ([mystery-word (first (hash-keys mystery-hash))]
@@ -37,13 +43,27 @@
                 (compare-two control-hash
                              (hash-remove mystery-hash mystery-word)
                              (hash-set results mystery-word (abs (- mystery-value (hash-ref control-hash mystery-word))))) 
-                (compare-two control-hash (hash-remove mystery-hash mystery-word) results)))))
-        
-  
-  (compare-two (hash-ref control-table (first (hash-keys control-table)))
-               (hash-ref input-mystery-table (first (hash-keys input-mystery-table)))
-               (hash)))
+                (compare-two control-hash
+                             (hash-remove mystery-hash mystery-word)
+                             (hash-set results mystery-word 0)))))) 
 
-(define (foo 
- (display (analyze-text (make-rarity-table '("test.txt" "test2.txt")(hash)) (make-rarity-table '("mysterytest.txt" "mysterytest2.txt")(hash))))
-;(display (hash-ref (make-rarity-table '("test.txt" "test2.txt")(hash)))     )
+  (let* ([differences (hash-values(compare-two control mystery (hash)))]
+         [sum    (apply + differences)]
+         [total  (length differences)])
+    (/ sum total)))
+
+;Gets the best match from two known texts, Doyle & Lovecraft, compared to the mystery text
+(define (get-best-match mystery-text-list)
+  (display (string-append "Analyzing " (first mystery-text-list) "\n"))
+  (let* ([mystery (get-rarity-hash (first mystery-text-list))]
+        [doyle (get-rarity-hash "doyle.txt")]
+        [doyle-score (analyze-text doyle mystery)]
+        [lovecraft (get-rarity-hash "lovecraft.txt")]
+        [lovecraft-score (analyze-text lovecraft mystery)])    
+    (display (string-append "\t" (first mystery-text-list) " is probably "))
+    (if (> doyle-score lovecraft-score) (display "Doyle \n") (display "Lovecraft \n"))
+    (if (equal? (length mystery-text-list) 1) (display "Done")
+    (get-best-match (rest mystery-text-list)))))
+
+;gets the best match for mystery texts.
+(get-best-match '("mystery1.txt" "mystery2.txt"))
